641e4f32c3ac0e40729b4d6013cd40ce5957cc1b
Iam maria theresa <iam.mariatheresaelkazzi@gmail.com>
2022-08-03 22:55:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+
+let companyCeo = 'Susan Smith';
+const companyName = 'Scuber';
+let mostProfitableNeighborhood = 'Chelsea';
+
+describe('index.js', function () {
+    describe('companyName', function () {
+        it('is set as Scuber', function () {
+            expect(companyName).to.equal('Scuber');
+        });
+
+    });
+    describe('mostProfitableNeighborhood', function () {
+        it('is declared as equal to Chelsea', function () {
+            expect(mostProfitableNeighborhood).to.equal('Chelsea');
+        });
+    });
+
+    describe('companyCeo', function () {
+        it('is declared as equal to Susan Smith', function () {
+            expect(companyCeo).to.equal('Susan Smith');
+        });
+    });
+});
